0cd0a8c1bdd9ac4abc3aedf5ca832e38bdec3363
Galina Moroz <kozinaksa@gmail.com>
2023-02-13 22:03:34 +0300

    refactor: make on webpack

---
+++ momentum/src/script.js
@@ -0,0 +1,7 @@
+import './index.html';
+import './libs/normalize.css';
+import './style/scss/fonts.scss';
+import './style/scss/style.scss';
+
+const mult = (a, b) => a * b;
+console.log(mult(4, 2));
